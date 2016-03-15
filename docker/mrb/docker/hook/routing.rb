6d3cea7b82b4a256c68fb3b0e0779054a45d201c
Narihiro Nakamura <authornari@gmail.com>
2016-03-15 19:12:33 +0900

    more readable

--- docker/mrb/docker/hook/routing.rb
+++ docker/mrb/docker/hook/routing.rb
@@ -1,19 +1,21 @@
-v = Nginx::Var.new
-user = v.remote_user
-if user.nil?
-  Nginx.errlogger Nginx::LOG_ERR, "user not found: user=<#{user}>"
-  hout = Nginx::Headers_out.new
-  hout["WWW-Authenticate"] = 'Basic realm="Restricted"'
-  Nginx.return Nginx::HTTP_UNAUTHORIZED
-else
+Nginx.return -> do
+  v = Nginx::Var.new
+  user = v.remote_user
+  if user.nil?
+    Nginx.errlogger Nginx::LOG_ERR, "user not found: user=<#{user}>"
+    hout = Nginx::Headers_out.new
+    hout["WWW-Authenticate"] = 'Basic realm="Restricted"'
+    return Nginx::HTTP_UNAUTHORIZED
+  end
+
   userdata = Userdata.new "redis_data_key"
   redis = userdata.redis
   upstream = redis.get(user || "")
-  if upstream.nil?
-    Nginx.errlogger Nginx::LOG_ERR, "upstream not found: user=<#{user}>"
-    Nginx.return Nginx::HTTP_UNAUTHORIZED
-  else
+  if upstream
     v.set "upstream", upstream
     Nginx.echo "Success"
+  else
+    Nginx.errlogger Nginx::LOG_ERR, "upstream not found: user=<#{user}>"
+    return Nginx::HTTP_UNAUTHORIZED
   end
-end
+end.call
